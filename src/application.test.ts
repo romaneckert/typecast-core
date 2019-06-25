e69910f62b61fe2b8b0884a588b7ac8c10afa209
Roman Eckert <mail@romaneckert.de>
2019-06-25 14:24:57 +0200

    change application test

--- src/application.test.ts
+++ src/application.test.ts
@@ -4,8 +4,4 @@ test('application', async () => {
     const app = new Application();
     await app.start();
     await app.stop();
-
-    const app2 = new Application([process.cwd()]);
-    await app.start();
-    await app.stop();
 });
