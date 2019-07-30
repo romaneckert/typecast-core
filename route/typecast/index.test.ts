e096b0a61bce65e14f3e779c7c624a8250a873c9
Roman Eckert <mail@romaneckert.de>
2019-07-30 23:58:16 +0200

    separate some tests

---
+++ route/typecast/index.test.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { Application } from '../../core/application';
+import Container from '../../core/container';
+import { HTTPServerConfig } from '../../config/http-server-config';
+const app: Application = new Application();
+
+beforeAll(async () => {
+    await app.start();
+});
+afterAll(async () => {
+    await app.stop();
+});
+
+test('/', async () => {
+    const httpServerConfig = await Container.get<HTTPServerConfig>(HTTPServerConfig);
+    const response = await axios.get(httpServerConfig.baseUrl);
+    expect(response.data.includes('Welcome to Typecast')).toBe(true);
+});
---
+++ route/typecast/install.test.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import url from 'url';
+import { Application } from '../../core/application';
+import Container from '../../core/container';
+import { HTTPServerConfig } from '../../config/http-server-config';
+import DatabaseService from '../../service/database';
+const app: Application = new Application();
+let database: DatabaseService;
+
+beforeAll(async () => {
+    await app.start();
+    database = await Container.get<DatabaseService>(DatabaseService);
+});
+afterAll(async () => {
+    await app.stop();
+});
+
+test('setup project', async () => {
+    // delete database to have a clean setup
+    await database.drop();
+
+    const httpServerConfig = await Container.get<HTTPServerConfig>(HTTPServerConfig);
+    const response = await axios.post(url.resolve(httpServerConfig.baseUrl, 'typecast/install'), { email: 'test@test.typecast' });
+    expect(response.data.includes('Please create your password')).toBe(true);
+    expect(response.data.includes('Click on the link in the email you received')).toBe(true);
+});
