c310ea961220c2b61f038b1df29410a8ea67d5a6
Roman Eckert <mail@romaneckert.de>
2019-08-01 20:13:29 +0200

    add database config test

---
+++ config/database-config.test.ts
@@ -0,0 +1,21 @@
+import Container from '../core/container';
+import Application from '../core/application';
+import DatabaseConfig from './database-config';
+
+const app: Application = new Application();
+let databaseConfig: DatabaseConfig;
+
+beforeAll(async () => {
+    await app.start();
+    databaseConfig = await Container.get<DatabaseConfig>(DatabaseConfig);
+});
+afterAll(async () => {
+    await app.stop();
+});
+
+test('database-config', async () => {
+    expect(typeof databaseConfig.database).toBe('string');
+    expect(typeof databaseConfig.host).toBe('string');
+    expect(() => databaseConfig.password).toThrow();
+    expect(() => databaseConfig.user).toThrow();
+});
