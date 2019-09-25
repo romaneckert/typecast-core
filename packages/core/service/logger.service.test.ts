9c905f14583b810c610c3ac49f551ad42089130d
Roman Eckert <mail@romaneckert.de>
2019-09-25 21:30:45 +0200

    speed up logger test

--- packages/core/service/logger.service.test.ts
+++ packages/core/service/logger.service.test.ts
@@ -5,13 +5,15 @@ describe('service', () => {
     test('logger', async () => {
         const logger = await ApplicationService.create<LoggerService>(LoggerService);
 
-        await logger.alert('alert message');
-        await logger.critical('critical message');
-        await logger.debug('debug message');
-        await logger.emergency('emergency message');
-        await logger.error('error message');
-        await logger.info('info message');
-        await logger.notice('notice message');
-        await logger.warning('warning message');
+        await Promise.all([
+            logger.alert('alert message'),
+            logger.critical('critical message'),
+            logger.debug('debug message'),
+            logger.emergency('emergency message'),
+            logger.error('error message'),
+            logger.info('info message'),
+            logger.notice('notice message'),
+            logger.warning('warning message'),
+        ]);
     });
 });
